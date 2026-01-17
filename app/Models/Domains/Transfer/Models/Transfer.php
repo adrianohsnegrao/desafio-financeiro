fe6e2f2d5908d3bbb0f93d3767fcdb0fc7902233
Adriano Negrao <adrianohsn2022@gmail.com>
2026-01-17 08:36:53 -0400

    feat: cria model e migration Transfer

---
+++ app/Models/Domains/Transfer/Models/Transfer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Domains\Transfer\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transfer extends Model
+{
+    protected $fillable = [
+        'payer_id',
+        'payee_id',
+        'amount',
+        'status',
+    ];
+}
